ff0483d787751918c38ae6b82898479f6c6d3600
prabhat <prabhatshukla0849@gmail.com>
2024-11-09 11:08:01 +0530

    FEAT: added types for OutGoing messages or messages to be broadcasted

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -6,44 +6,50 @@ interface User {
   conn: connection;
 }
 
-interface Room{
-  users:User[]
+interface Room {
+  users: User[]
 }
 
 //Map is a class which can be initialized 
-export class UserManager{
-    private rooms: Map<string, Room>;
-    constructor(){
-      this.rooms = new Map<string,Room>()
-    }
-
-    addUser(name: string,userId:string, roomId:string,socket:connection){
-       if(!this.rooms.get(roomId)){
-        this.rooms.set(roomId , {
-          users:[]
-        })
-       }
-       this.rooms.get(roomId)?.users.push({
-        id:userId,
-        name,
-        conn:socket
-       })
-      //connection object has various callbacks which are inbuilt in them , here in websocket connection object reasonCode and description are two callbacks
-       socket.on("close", (reasonCode, description) =>{
-        this.removeUser(roomId,userId)
-       })
-    }
-    removeUser(userId:string, roomId:string){
-      const users = this.rooms.get(roomId)?.users
-      if(users){
-        users.filter(({id})=>id!==userId)
-      }
-    }
-
-    getUser(roomId: string, userId: string): User | null {
-      const user = this.rooms.get(roomId)?.users.find((({id}) => id === userId));
-      return user ?? null;
+export class UserManager {
+  private rooms: Map<string, Room>;
+  constructor() {
+    this.rooms = new Map<string, Room>()
   }
+
+  addUser(name: string, userId: string, roomId: string, socket: connection) {
+    if (!this.rooms.get(roomId)) {
+      this.rooms.set(roomId, {
+        users: []
+      })
+    }
+    this.rooms.get(roomId)?.users.push({
+      id: userId,
+      name,
+      conn: socket
+    })
+    //connection object has various callbacks which are inbuilt in them , here in websocket connection object reasonCode and description are two callbacks
+    socket.on("close", (reasonCode, description) => {
+      this.removeUser(roomId, userId)
+    })
+  }
+  removeUser(userId: string, roomId: string) {
+    const users = this.rooms.get(roomId)?.users
+    if (users) {
+      users.filter(({ id }) => id !== userId)
+    }
+  }
+
+  getUser(roomId: string, userId: string): User | null {
+    const user = this.rooms.get(roomId)?.users.find((({ id }) => id === userId));
+    return user ?? null;
+  }
+
+  broadCast(userId: string , roomId: string){
+    const user = this.get
+  }
+
+
 
 
 }
---
+++ src/messages/outgoingMessages.ts
@@ -0,0 +1,24 @@
+
+export enum SupportedMessage {
+  AddChat =  "ADD_CHAT",
+  UpdateChat = "UPDATE_CHAT",
+}
+
+type MessagePayload = {
+  roomId: string;
+  message: string;
+  name: string;
+  upvotes: number;
+  chatId: string;
+}
+
+export type OutgoingMessage = {
+  type : SupportedMessage.AddChat,
+  payload: MessagePayload
+} | {
+  type : SupportedMessage.UpdateChat,
+  payload:Partial<MessagePayload>
+}
+
+
+// In TypeScript, Partial is a utility type that takes an object type and makes all of its properties optional. It’s particularly useful when you want to create a version of a type where some or all properties may not be present, often in situations like partial updates or initializing objects with default values.
